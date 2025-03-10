b5d0ae5f1bf7f1d8b42f845e6a6610a54fc18a7e
kumiland <kumisando@kumis-macbook-air2024.local>
2025-03-10 13:16:14 +0900

    Initial commit (All checked)

---
+++ .gradle/Markdown.md
@@ -0,0 +1,47 @@
+
+//クラス図
+```mermaid
+classDiagram
+class BankAccount
+BankAccount : +String owner
+BankAccount : +BigDecimal balance
+BankAccount : +deposit(amount)
+BankAccount : +withdrawal(amount)
+```
+
+
+
+
+//ER図
+```mermaid
+erDiagram
+    CUSTOMER ||--o{ ORDER : places
+    CUSTOMER {
+        string name
+        string custNumber
+        string sector
+    }
+    ORDER ||--|{ LINE-ITEM : contains
+    ORDER {
+        int orderNumber
+        string deliveryAddress
+    }
+    LINE-ITEM {
+        string productCode
+        int quantity
+        float pricePerUnit
+    }
+```
+
+
+
+
+
+//シーケンス図
+```mermaid
+sequenceDiagram
+    Alice->>+John: Hello John, how are you?
+    Alice->>+John: John, can you hear me?
+    John-->>-Alice: Hi Alice, I can hear you!
+    John-->>-Alice: I feel great!
+```
---
+++ src/main/java/plugin/enemydown/Main.java
@@ -0,0 +1,27 @@
+package plugin.enemydown;
+
+import java.util.Objects;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import plugin.enemydown.command.EnemyDownCommand;
+import plugin.enemydown.command.EnemySpawnCommand;
+
+
+public final class Main extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    EnemyDownCommand enemyDownCommand = new EnemyDownCommand(this);
+    Bukkit.getPluginManager().registerEvents(enemyDownCommand,this);
+    getCommand("enemyDown").setExecutor(enemyDownCommand);
+
+    EnemySpawnCommand enemySpawnCommand = new EnemySpawnCommand(this);
+    Bukkit.getPluginManager().registerEvents(enemySpawnCommand,this);
+    getCommand("enemySpawn").setExecutor(enemySpawnCommand);
+  }
+}
+
+
